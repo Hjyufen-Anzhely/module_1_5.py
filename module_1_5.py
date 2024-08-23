2c376c0db95cbe1eb808d5e92cf08a870a493be1
Hjyufen-Anzhely <anzh.zimina@yandex.ru>
2024-08-23 06:32:31 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = [1, 2], True, 'pen'
+print (immutable_var)
+immutable_var[0][0] = 2
+print (immutable_var)
+mutable_list = ['pen', 'pencil', 'wood']
+mutable_list.remove ('wood')
+print (mutable_list)
